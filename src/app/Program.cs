3f609ac878f6b2dbe7040cbaebffdfd11fb61956
James Ruiz <fallen@Jamess-MacBook-Air.local>
2016-08-20 13:07:39 -0400

    Initial commit of C# Mac Tutorial

---
+++ src/app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+using Library;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteLine($"The answer is {new Thing().Get(19, 23)}");
+        }
+    }
+}
